eefb94db11cc6ba56bdda829c564c8d1e9a9734e
Deepak Katariya <deepak.sorthiya@gmail.com>
2021-08-15 01:13:10 +0530

    Initial commit

---
+++ src/main/java/com/example/rest/ApiController.java
@@ -0,0 +1,15 @@
+package com.example.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.ZonedDateTime;
+
+@RestController
+public class ApiController {
+
+    @GetMapping
+    public ZonedDateTime getZonedDateTime() {
+        return ZonedDateTime.now();
+    }
+}
