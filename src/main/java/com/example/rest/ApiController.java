55c1d9df74bed1a53a9c338170c61a1b937dc2b3
Deepak Katariya <deepak.sorthiya@gmail.com>
2021-08-15 01:22:07 +0530

    Comment added

--- src/main/java/com/example/rest/ApiController.java
+++ src/main/java/com/example/rest/ApiController.java
@@ -11,6 +11,7 @@ public class ApiController {
     @GetMapping
     public ZonedDateTime getZonedDateTime() {
         //I am from local
+        //I am Deepak
         return ZonedDateTime.now();
     }
 }
