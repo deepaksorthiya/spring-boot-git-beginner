d7d583de17d5e26109ecf177ac220c1d1b550c49
Deepak Katariya <deepak.sorthiya@gmail.com>
2021-10-09 23:28:01 +0530

    ide-local

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -4,10 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//ide-local
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        //I am from ide-local
+        //I am from ide-local
+        //I am from ide-local
+        //I am from ide-local
+        //I am from ide-local
+        //I am from ide-local
     }
 
 }
