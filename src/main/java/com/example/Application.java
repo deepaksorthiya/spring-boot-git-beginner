21fd111098706503586653789467e3bdcf0a2730
Deepak Katariya <deepak.sorthiya@gmail.com>
2021-10-09 21:44:44 +0530

    Update Application.java

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -4,17 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//gggg
+//ffff
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        //I am from remote
-        //I am from remote
-        //I am from remote
-        //I am from remote
-        //I am from remote
-        //I am from remote
+        //I am from ffff
+        //I am from ffff
+        //I am from ffff
+        //I am from ffff
+        //I am from ffff
+        //I am from ffff
     }
 
 }
