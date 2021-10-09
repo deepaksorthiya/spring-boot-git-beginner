f5e8f8c25e16e67e2ae86cefc4acb378df188c01
Deepak Katariya <deepak.sorthiya@gmail.com>
2021-10-09 21:22:58 +0530

    local changes

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -8,12 +8,12 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        //I am from remote
-        //I am from remote
-        //I am from remote
-        //I am from remote
-        //I am from remote
-        //I am from remote
+        //I am from local
+        //I am from local
+        //I am from local
+        //I am from local
+        //I am from local
+        //I am from local
     }
 
 }
