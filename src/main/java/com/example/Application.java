e8469d3d299d17326b0624b00a6c9e3018d59301
Deepak Katariya <deepak.sorthiya@gmail.com>
2021-10-09 21:22:58 +0530

    local changes

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -4,16 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//gggg
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        //I am from remote
-        //I am from remote
-        //I am from remote
-        //I am from remote
-        //I am from remote
-        //I am from remote
+        //I am from local
+        //I am from local
+        //I am from local
+        //I am from local
+        //I am from local
+        //I am from local
     }
 
 }
