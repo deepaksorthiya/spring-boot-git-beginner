bfa5f01e2f2b65217f8f0fbc744644b0bc8ec8bc
Deepak Katariya <deepak.sorthiya@gmail.com>
2021-12-23 18:57:06 +0530

    n2

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,5 +1,5 @@
 package com.example;
-
+import org.springframework.boot.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
+        //I am from local
         SpringApplication.run(Application.class, args);
     }
 
