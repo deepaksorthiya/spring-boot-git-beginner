383f54396df2f7c261bbe3a404444a2003598386
Deepak Katariya <deepak.sorthiya@gmail.com>
2021-12-23 17:37:08 +0530

    reset commit

--- src/main/java/com/example/rest/ApiController.java
+++ src/main/java/com/example/rest/ApiController.java
@@ -3,16 +3,14 @@ package com.example.rest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 
 @RestController
 public class ApiController {
 
     @GetMapping
-    public ZonedDateTime getZonedDateTime() {
-        //I am from local
-        //I am Deepak
-        //I am from server
-        return ZonedDateTime.now();
+    public LocalDateTime getZonedDateTime() {
+        return LocalDateTime.now();
     }
 }
