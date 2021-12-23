5647286384c0df71e36fbc5b7070d2360d28c9c3
Deepak Katariya <deepak.sorthiya@gmail.com>
2021-12-23 19:05:05 +0530

    pp

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,5 +1,5 @@
 package com.example;
-import org.springframework.boot.*;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-        // I am from remote
         SpringApplication.run(Application.class, args);
     }
 
