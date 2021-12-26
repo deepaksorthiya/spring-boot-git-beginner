5cf017ef8de335908635f9e21206f5a5b21c08fd
Deepak Katariya <deepak.sorthiya@gmail.com>
2021-12-26 17:24:49 +0530

    feature---

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,12 +1,14 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
+        // I am local
         SpringApplication.run(Application.class, args);
     }
 
