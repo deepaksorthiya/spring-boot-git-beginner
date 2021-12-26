88275906c088db256d29be3d22cd5963489eb76d
Deepak Katariya <deepak.sorthiya@gmail.com>
2021-12-26 17:23:45 +0530

    Update Application.java

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -2,11 +2,13 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
+        // I am remote
         SpringApplication.run(Application.class, args);
     }
 
